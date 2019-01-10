cc88c43a3d2b8a68dac6ee61e27ad142fcc26d4e
void-sister <el.varvara.el@gmail.com>
2019-01-10 12:15:49 +0200

    updating and deleting articles

--- index.js
+++ index.js
@@ -84,6 +84,44 @@ app.post('/articles/add', function(req, res){
   });
 });
 
+//load edit form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
+
+//edit submit POST route
+app.post('/article/edit/:id', function(req, res){
+  let article = {}; //define an empty object
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect('/');
+  });
+});
+
+app.delete('/article/:id', function(req, res){
+  let query = {_id:req.params.id}
+  Article.remove(query, function(err){
+    if (err) {
+      console.log(err);
+    }
+    res.send('Success');
+  });
+});
+
 //start server
 app.listen(3000, function() {
   console.log('Server started on port 3000.');
